fe013c8a063aabe1d4195fed996232f31444dd43
Ismael <ismael2021ab@gmail.com>
2023-09-23 17:56:27 -0300

    Feat: Build Complete

--- hex_dec.c
+++ hex_dec.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
 int main()
 {
+    char hex[100];
+    int value, lstValues[100], sum = 0;
 
-    
+    printf("Digite o Hexadecimal: ");
+    scanf("%s", hex);
 
+    strupr(hex);
+    strrev(hex);
+
+
+    for (int i = 0; i < strlen(hex); ++i)
+    {
+
+        if (isdigit(hex[i]))
+        {
+            value = hex[i] - '0';
+            lstValues[i] = value;
+        }
+        else
+        {
+            value = hex[i] - 'A' + 10;
+            lstValues[i] = value;
+
+        }
+
+
+    }
+
+    for (int i = 0; i < strlen(hex); ++i)
+    {
+        sum += (lstValues[i]) * (pow(16, i));
+    }
+    printf("\nResultado: %d", sum);
 }

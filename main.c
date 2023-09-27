7f4d902d0c928621067af377d571bd0f902adca5
Ismael <ismael2021ab@gmail.com>
2023-09-27 16:14:46 -0300

    Created: main.c
    Unifying Codes

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int bin_dec(char num);
+int dec_bin(int num);
+int hex_dec(char num);
+int dec_hex(int num);
+int oct_dec(char num);
+int dec_oct(int num);
+
+
+int main()
+{
+
+}
